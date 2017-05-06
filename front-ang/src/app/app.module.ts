f24a02c52990a15604ee2ab92b219635335f47d9
murat onur karadeniz <muratonurkaradeniz@outlook.com>
2017-05-06 19:18:58 +0300

    ang

---
+++ front-ang/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+ 
+import { AuthModule } from './auth/auth.module';
+ 
+import { HomeModule } from './home/home.module';
+import { ProfileModule } from './profile/profile.module';
+import { SettingsModule } from './settings/settings.module';
+import {
+  ApiService,
+  ArticlesService,
+  AuthGuard,
+  CommentsService,
+  FooterComponent,
+  HeaderComponent,
+  JwtService,
+  ProfilesService,
+  SharedModule,
+  TagsService,
+  UserService
+} from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+   
+    AuthModule, 
+    HomeModule,
+    ProfileModule,
+    rootRouting,
+    SharedModule,
+    SettingsModule
+  ],
+  providers: [
+    ApiService,
+    ArticlesService,
+    AuthGuard,
+    CommentsService,
+    JwtService,
+    ProfilesService,
+    TagsService,
+    UserService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front-ang/src/app/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { ListErrorsComponent } from './list-errors.component';
+import { ShowAuthedDirective } from './show-authed.directive';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule
+  ],
+  declarations: [
+   
+    ListErrorsComponent,
+    ShowAuthedDirective
+  ],
+  exports: [
+
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ListErrorsComponent,
+    RouterModule,
+    ShowAuthedDirective
+  ]
+})
+export class SharedModule {}
